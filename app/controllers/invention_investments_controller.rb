cc26edd0a0afe47ea92df05ba51fa0fbb23ce94b
filmscore88 <cmtmorales24@gmail.com>
2019-06-06 09:30:41 -0500

    built invention_investments controller and view pages

--- app/controllers/invention_investments_controller.rb
+++ app/controllers/invention_investments_controller.rb
@@ -1,23 +1,34 @@
 class InventionInvestmentsController < ApplicationController
   def new
-    @investment= Investment.new
+    @invention_investment= InventionInvestment.new
   end
 
   def create
+    #binding.pry
+    @invention_investment= InventionInvestment.create(invention_investment_params)
+    if @invention_investment
 
+      redirect_to invention_investment_path(@invention_investment)
+    else
+      render :new
+    end
   end
 
   def show
+    #binding.pry
+    @invention_investment= InventionInvestment.find_by(id: params[:id])
+
 
   end
 
   def index
+    @invention_investments= InventionInvestment.all
   end
 
   private
 
-  def investment_investment_params
-    params.require(:invention_investments).permit(:amount, :investor_id, :inventor_id, :invention_id)
-  end 
+  def invention_investment_params
+    params.require(:invention_investment).permit(:id, :amount, :investor_id, :invention_id, :inventor_id)
+  end
 
 end
--- app/controllers/investors_controller.rb
+++ app/controllers/investors_controller.rb
@@ -1,20 +1,29 @@
 class InvestorsController < ApplicationController
 
   def new
+    @investor= Investor.new
 
   end
 
   def create
-
+    @investor= Investor.create(investor_params)
+    if @investor
+      redirect_to investor_path(@investor)
+    else
+      render :new
+    end
   end
 
   def show
+    
+    @investor= Investor.find_by(id: params[:id])
+
 
   end
 
   private
 
   def investor_params
-    params.require(:investor).permit(:name, :email)
-  end 
+    params.require(:investor).permit(:id, :name, :email)
+  end
 end
